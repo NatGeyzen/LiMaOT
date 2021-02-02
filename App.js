f4aee1e639765508af3f8c224386f36b68eaa765
Nat Geyzen <nat.geyzen@outlook.com>
2021-02-02 10:48:49 -0600

    installed packages for use of custom fonts, icons and linear gradients, necessary changes to App.js so custom fonts can be used in entire app

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts, RussoOne_400Regular } from '@expo-google-fonts/russo-one';
+import { Roboto_400Regular, Roboto_500Medium_Italic, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+
 import store from './store/store';
 
+
 export default function App() {
-  	return (
-		<Provider store={store}>
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<StatusBar style="auto" />
-    		</View>
-		</Provider>
-  	);
+
+	let [fontsLoaded] = useFonts({
+		RussoOne_400Regular,
+		Roboto_400Regular,
+		Roboto_500Medium_Italic,
+		Roboto_700Bold
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return (
+			<Provider store={store}>
+				<View style={styles.container}>
+					<Text>App.js</Text>
+					<StatusBar style="auto" />
+				</View>
+			</Provider>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	backgroundColor: 'lightgrey'
   },
 });
