b17eb9b2f3c9d04a8aadad826310ca8924570770
Nat Geyzen <nat.geyzen@outlook.com>
2021-02-02 10:54:24 -0600

    new reducers and state property added to redux user slice

--- store/store.js
+++ store/store.js
@@ -4,7 +4,7 @@ import user from './user';
 
 const store = configureStore({
     reducer: {
-        userReducer: user,
+        user: user,
     }
 });
 
--- store/user.js
+++ store/user.js
@@ -4,14 +4,15 @@ const user = createSlice({
     name: 'user',
     initialState: {
         userId: null,
-        isNewUser: false
+        isReturningUser: null
     },
     reducers: {
-        switchUserMode: (state, action) => state.isNewUser = !state.isNewUser
+        setUserMode: (state, action) => { action.payload === 'returning' ? state.isReturningUser = true : state.isReturningUser = false},
+        switchUserMode: (state) => {state.isReturningUser = !state.isReturningUser},
+        authUser: (state, action) => state.authData = action.payload,
+        authUserSuccess: (state, action) => state.userId = action.payload
     }
 });
 
-const { actions, reducer } = user;
-export const { switchUserMode } = actions;
-export default reducer;
-
+export const { setUserMode, switchUserMode, authUser, authUserSuccess } = user.actions;
+export default user.reducer;
